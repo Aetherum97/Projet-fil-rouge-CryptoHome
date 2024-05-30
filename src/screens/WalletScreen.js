757a54c1e6ee45152f3f5157e84de6f91002ac18
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-05-30 16:07:49 +0200

    maj Screen

--- src/screens/WalletScreen.js
+++ src/screens/WalletScreen.js
@@ -1,12 +1,20 @@
+import OrangeLineComponent from "../components/OrangeLineComponent.js";
 import Screen from "./Screen.js";
 
 export default class WalletScreen extends Screen {
+  constructor(){
+    super();
+    this.prop.orangeLineComponent = new OrangeLineComponent();
+  }
   render() {
     return `
     <header>
       <navbar-component/>
     </header>
     <h1>Wallet en cours de création...</h1>
+    <div>
+      <orangeline-component/>
+    </div>
     <footer>
       <footerdesktop-component/>
     </footer>
