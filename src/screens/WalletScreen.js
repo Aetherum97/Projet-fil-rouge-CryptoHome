c2546bb2939e50aa7b7b4f769ca5d2c05d2bcc1f
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-06-06 10:38:12 +0200

    ajout tableau tabulator + fonctionnalité fiscalité

--- src/screens/WalletScreen.js
+++ src/screens/WalletScreen.js
@@ -1,10 +1,12 @@
 import OrangeLineComponent from "../components/OrangeLineComponent.js";
+import TaxationScreenComponent from "../components/TaxationScreenComponent.js";
 import Screen from "./Screen.js";
 
 export default class WalletScreen extends Screen {
   constructor(){
     super();
     this.prop.orangeLineComponent = new OrangeLineComponent();
+    this.prop.taxationScreenComponent = new TaxationScreenComponent();
   }
   render() {
     return `
@@ -14,6 +16,9 @@ export default class WalletScreen extends Screen {
     <h1>Wallet en cours de création...</h1>
     <div>
       <orangeline-component/>
+    </div>
+    <div>
+    <taxationscreen-component/>
     </div>
     <footer>
         <div>
